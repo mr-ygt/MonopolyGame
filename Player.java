7c5215996e7bb67784de0366d7be1738c260760a
Berk Yıldız <berkyildiz5299@gmail.com>
2019-11-07 21:27:59 +0000

    Upload New File

---
+++ Player.java
@@ -0,0 +1,38 @@
+
+public class Player extends MonopolyGame {
+
+    // Money, piece , sequence of player
+    private int money;
+    private int piece;
+    private int sequence;
+
+    // Default constructor
+    public Player() {
+
+    }
+
+    // Constructor with parameters
+    public Player(int money, int piece, int sequence) {
+        this.money = money;
+        this.piece = piece;
+        this.sequence = sequence;
+    }
+
+    // Method for rolling 2 dice, it returns number between 1 and 12.
+    private static int dice() {
+        int dice = (int) Math.floor(Math.random() * 12);
+        while (dice == 0) {
+            dice = (int) Math.floor(Math.random() * 12);
+        }
+        return dice;
+    }
+
+    // Rolling dice
+    private static int dice = dice();
+
+    // Getter method for private variable
+    public static int getDice() {
+        return dice;
+    }
+
+}
