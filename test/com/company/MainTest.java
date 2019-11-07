a4e78ca8b546b3795d66a56d542dfd0bf50d0c4c
Emre YİĞİT <donemreyigit@gmail.com>
2019-11-07 23:28:34 +0300

    second

---
+++ test/com/company/MainTest.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MainTest {
+
+    @Test
+    public void x() {
+
+    }
+}
