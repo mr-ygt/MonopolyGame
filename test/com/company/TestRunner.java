d7daaf183401a56e1b8442a1267b6e9f9fe91add
Mustafa Selimoğlu <selimoglu.mustafa@outlook.com>
2019-11-08 19:53:06 +0300

    Main added and Test added

--- test/com/company/TestRunner.java
+++ test/com/company/TestRunner.java
@@ -1,4 +1,19 @@
 package com.company;
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+
+import java.util.List;
 
 public class TestRunner {
+
+
+    public static void main(String[] args) {
+        Result result = JUnitCore.runClasses(TestMain.class);
+
+        for (Failure failure : result.getFailures()) {
+            System.out.println(failure.toString());
+        }
+        System.out.println(result.wasSuccessful());
+    }
 }
