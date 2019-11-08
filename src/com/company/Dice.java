c361a0ade55c047b0704d17ba6dedab9b7af3596
Emre YİĞİT <donemreyigit@gmail.com>
2019-11-08 04:00:40 +0300

    I created three java files.

---
+++ src/com/company/Dice.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import java.util.Random;
+
+public class Dice {
+
+    public Random rand;
+    public int dice;
+
+    public Dice(){
+        rand = new Random();
+    }
+
+}
