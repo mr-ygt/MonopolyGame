b423654af29df8146ee7ee26e9765211b19c3523
Furkan Kemal DÖNMEZ <furkankemal.17@gmail.com>
2019-12-05 18:34:10 +0000

    Update GoSquare.java

--- src/com/company/GoSquare.java
+++ src/com/company/GoSquare.java
@@ -4,7 +4,7 @@ public class GoSquare extends Square {
 
     int goMoney;
 
-    public GoSquare(String name, int goMoney){
+    public GoSquare(String name, boolean purchasable, int goMoney){
         super(name, false, 0);
         this.goMoney = goMoney;
         this.position = 0;
@@ -17,7 +17,17 @@ public class GoSquare extends Square {
         this.goMoney = goMoney;
     }
 
+    @Override
     public void Speak(){
         System.out.println("Name of the square is: Start");
     }
+
+	@Override
+	public void action(Player player, Board board) {
+
+		board.squares[player.piece.position].Speak();
+        System.out.println("So, " + player.name + " earn " + goMoney + "$");
+        player.addBalance(goMoney);
+
+	}
 }
