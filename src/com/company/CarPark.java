56c673ad326ef9279e6e7acd91d11c2b2de6b0e1
Furkan Kemal DÖNMEZ <furkankemal.17@gmail.com>
2019-12-05 18:29:34 +0000

    Update CarPark.java

--- src/com/company/CarPark.java
+++ src/com/company/CarPark.java
@@ -1,8 +1,22 @@
 package com.company;
 
 public class CarPark extends Square {
-    @Override
-    public void Speak() {
 
-    }
+	public CarPark(String name, boolean purchasable, int position) {
+		super(name, false, position);
+	}
+
+	@Override
+	public void Speak() {
+		System.out.println("Name of the square is: CarPark");
+
+	}
+
+	@Override
+	public void action(Player player, Board board) {
+
+		board.squares[player.piece.position].Speak();
+
+	}
+
 }
