e656d5554f896c8ec3c5d00c9040ad06e1830dd8
Furkan Kemal DÖNMEZ <furkankemal.17@gmail.com>
2019-12-06 17:37:19 +0000

    Update TaxSquare.java

--- src/com/company/TaxSquare.java
+++ src/com/company/TaxSquare.java
@@ -5,7 +5,7 @@ public class TaxSquare extends Square {
     int taxAmount;
 
     public TaxSquare(String name, boolean purchasable, int position, int taxAmount) {
-		super(name, false, position);
+		super(name, purchasable, position);
 		this.taxAmount = taxAmount;
 	}
 
@@ -13,7 +13,7 @@ public class TaxSquare extends Square {
         return taxAmount;
     }
     public void setTaxAmount(int tax){
-        this.taxAmount = tax;
+        taxAmount = tax;
     }
 
     @Override
@@ -23,9 +23,10 @@ public class TaxSquare extends Square {
 
 	@Override
 	public void action(Player player, Board board) {
-		board.squares[player.piece.position].Speak();
-        System.out.println("So, " + player.name + " has to give " + taxAmount + "$ for tax");
+		Speak();
+        System.out.println("So, " + player.getName() + " has to give " + taxAmount + "$ for tax");
         player.reduceBalance(taxAmount);
 
+        System.out.println("Current balance of " + player.getName() + " is " + player.getMoney().getAmount() + "$");
 	}
 }
