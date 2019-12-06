bdd61f279cf3bdbe916e60c51f99e2e6210d9ab7
Furkan Kemal DÖNMEZ <furkankemal.17@gmail.com>
2019-12-06 17:35:07 +0000

    Update JailSquare.java

--- src/com/company/JailSquare.java
+++ src/com/company/JailSquare.java
@@ -3,19 +3,20 @@ package com.company;
 public class JailSquare extends Square {
 
     public JailSquare(String name, boolean purchasable, int position) {
-		super(name, false, position);
+		super(name, purchasable, position);
 	}
 
     @Override
 	public void Speak(){
-        System.out.println("Name of the square is: Jail");
+        System.out.println("Name of the square is: " + getName());
     }
 
 	@Override
 	public void action(Player player, Board board) {
 
-		board.squares[player.piece.position].Speak();
-		System.out.println("So, " + player.name + " can't move ");
-
+		Speak();
+		System.out.println("So, " + player.getName() + " can't move ");
+		owner.isInJail = true;
+		System.out.println("Current balance of " + player.getName() + " is " + player.getMoney().getAmount() + "$");
 	}
 }
