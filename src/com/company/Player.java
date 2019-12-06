0124fa1f7b0c68669819ecf4e8e34dd0c3799c39
Furkan Kemal DÖNMEZ <furkankemal.17@gmail.com>
2019-12-06 18:04:13 +0000

    Update Player.java

--- src/com/company/Player.java
+++ src/com/company/Player.java
@@ -7,8 +7,11 @@ public class Player implements Comparable<Player> {
     Money money;
     public Piece piece;
     public int twiceCounter = 0;
-    public boolean isDouble = true;
-    public int balance;
+    public boolean isDouble = false;
+    public boolean isInJail = false ;
+
+    private int Utility;
+    private int RailRoad;
 
     public Player(){
 
@@ -20,10 +23,7 @@ public class Player implements Comparable<Player> {
         this.name = name;
         this.piece = piece;
     }
-    public void getCard(Card card, Board board){
-        //int type = card.type;
-        card.action(this, board);
-    }
+
     //these two methods set the balance.
     public void reduceBalance(int amount){
         this.money.subtractMoney(amount);
@@ -32,40 +32,24 @@ public class Player implements Comparable<Player> {
         this.money.addMoney(amount);
     }
 
-    public int move(Piece piece, int dices){   //this method rolls the dices and moves the piece of player based on result
-        int step = this.roll(dices);                  //and returns the new position of piece of player.
+    public int move(Piece piece, Dice dice1, Dice dice2){   //this method rolls the dices and moves the piece of player based on result
+        int step = this.roll(dice1,dice2);                  //and returns the new position of piece of player.
         this.piece.position += step;
         this.piece.position = (this.piece.position%40);
         System.out.println(this.name + " is now located in Square " + this.piece.position);
         return piece.position;
     }
 
-    public int roll(int dices){        //this method rolls the dices and returns the sum of them.
-        int faces[] = new int[dices];
-        int total = 0;
-        Dice dice = new Dice();
-        for(int i = 0; i < dices; i++){
-            faces[i] = (dice.rand.nextInt(6) + 1);
-        }
-        for(int i = 0; i < faces.length-1; i++){
-            if(faces[i] != faces[i+1]){
-                this.isDouble = false;
-                break;
-            }
-            else{
-                this.isDouble = true;
-            }
-        }
-        if(this.isDouble == true){
-            this.twiceCounter++;
-        }
-        for(int i = 1; i < faces.length+1; i++){
-            System.out.print("The " + i + ". dice: " + faces[i-1]);
-            total += faces[i-1];
-        }
-        System.out.println(" and the sum is " + total);
+    public int roll(Dice dice1, Dice dice2){        //this method rolls the dices and returns the sum of them.
+        int face1 = (dice1.rand.nextInt(6) + 1);
+        int face2 = (dice2.rand.nextInt(6) + 1);
 
-        return total;
+        if(face1 == face2){
+            this.twiceCounter++;
+            this.isDouble = true;
+        }
+        System.out.println("First dice: " + face1 + ", the Second dice: " + face2 + " and the sum is " + (face1 + face2));
+        return (face1 + face2);
     }
 
     public void Speak(){
@@ -78,8 +62,27 @@ public class Player implements Comparable<Player> {
     public int compareTo(Player o) {
         return this.money.getAmount() - o.money.getAmount();
     }
-    public String getName(){return name;}
+
+    public String getName(){
+    	return name;}
+
     public Money getMoney() {
 		return money;
+	}
+
+	public int getUtility() {
+		return this.Utility;
+	}
+
+	public void addUtility(int utility) {
+		this.Utility += utility;
+	}
+
+	public int getRailRoad() {
+		return this.RailRoad;
+	}
+
+	public void addRailRoad(int railRoad) {
+		this.RailRoad += railRoad;
 	}
 }
